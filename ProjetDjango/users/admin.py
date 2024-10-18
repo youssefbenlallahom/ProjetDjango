a64545e3430682dc4a51823869525fea8574c42d
youssefbello <youssef.benlallahom@esprit.tn>
2024-10-18 10:25:50 +0100

    commited

--- ProjetDjango/users/admin.py
+++ ProjetDjango/users/admin.py
@@ -29,6 +29,18 @@ class ParticipantAdmin(admin.ModelAdmin):
         queryset = super().get_queryset(request)
         print(queryset)
         return queryset.order_by('-is_superuser', 'username')
-    
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display=['reservation_date', 'conference' , 'participant' , 'confirmed']
+    actions = ['confirmed']
+    def confirmed (self,request,queryset):
+        queryset.update(confirmed =True)
+        self.message_user(request,"Les reservations sont confirmes")
+    confirmed.short_description = "Reservation a non confirmes"
+    def unconfirmed (self,request,queryset):
+        queryset.update(confirmed =False)
+        self.message_user(request,"Les reservations ne sont pas confirmes")
+    unconfirmed.short_description = "Reservation a  confirmes"
+   
 admin.site.register(Participant,ParticipantAdmin)
-admin.site.register(Reservation)
+admin.site.register(Reservation,ReservationAdmin)
